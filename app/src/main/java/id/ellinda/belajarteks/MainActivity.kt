a5d4ad219b9f9d23aa8ace3b4f8049250a2ee716
ellindaa <ellinda.ellinda@students.utdi.ac.id>
2024-10-03 18:10:41 +0700

    initial update

--- app/src/main/java/id/ellinda/belajarteks/MainActivity.kt
+++ app/src/main/java/id/ellinda/belajarteks/MainActivity.kt
@@ -3,6 +3,7 @@ package id.ellinda.belajarteks
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,10 +14,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import id.ellinda.belajarteks.ui.theme.BelajarTeksTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,16 +35,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
+                    GreetingImage(
                         message = "Happy Birthday Mama!",
-                        from = "From Ellinda",
-                        modifier = Modifier.padding(8.dp)
+                        from = "From Ellinda"
                     )
                 }
             }
         }
     }
 }
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box(modifier) {
+        Image(
+            painter = image,
+            // Deskripsi gambar
+            contentDescription = null,
+            // Mengatur ukuran gambar
+            contentScale = ContentScale.Crop,
+            // Mengatur opacity gambar
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
 // Membuat fungsi GreetingText dengan parameter massage jenis string,
 // parameter from jenis string, dan parameter modifier jenis Modifier
 @Composable
@@ -72,7 +101,11 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     BelajarTeksTheme(       ) {
-        // Memanggil fungsi GreetingText untuk di tampilkan pada pratinjau
-        GreetingText(message = "Happy Birthday Mama!", from = "From Ellinda")
+        // Memanggil fungsi GreetingText untuk di tampilkan pada pratinjau,
+        GreetingImage(
+            message = "Happy Birthday Mama!",
+            from = "From Ellinda")
     }
 }
+
+
