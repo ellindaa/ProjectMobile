171066cd0589b15aed1edbf583a2bd83cab718eb
ellindaa <ellinda.ellinda@students.ac.id>
2024-10-21 11:48:37 +0700

    initial commit

---
+++ app/src/main/java/id/ellinda/mygrid/model/Topic.kt
@@ -0,0 +1,10 @@
+package id.ellinda.mygrid.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
